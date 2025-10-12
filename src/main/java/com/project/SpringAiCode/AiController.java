fbf908b37d5c0291810dd37cc787a7030e7cde6d
Prashant Choudhary <prash.sastra1@gmail.com>
2025-10-12 21:36:05 +0530

    Java AI Development on chat client

--- src/main/java/com/project/SpringAiCode/AiController.java
+++ src/main/java/com/project/SpringAiCode/AiController.java
@@ -1,22 +1,40 @@
 package com.project.SpringAiCode;
 
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AiController {
-    private final OpenAiChatModel chatModel;
+    private final ChatClient chatClient;
 
     public AiController(OpenAiChatModel chatModel) {
-        this.chatModel = chatModel;
+        this.chatClient = ChatClient.create(chatModel);
     }
 
     @GetMapping("/api/{message}")
-    public String getResponseFromPrompt(@PathVariable String message){
-        return chatModel.call(message);
+    public ResponseEntity<String> getResponseFromPrompt(@PathVariable String message){
+        String response = "";
+        ChatResponse chatResponse = chatClient
+                .prompt(message)
+                .call()
+                .chatResponse();
+        if (chatResponse != null){
+            System.out.println(chatResponse.getMetadata().getModel());
+            response = chatResponse
+                    .getResult()
+                    .getOutput()
+                    .getText();
+            return ResponseEntity.ok(response);
+        }
+        return ResponseEntity.ok("No Response To Send...");
+//        return ResponseEntity.ok(chatClient.prompt(message).call().content());
+//        return chatModel.call(message);
 //        ChatModel is abstracted way to call open AI API, message is our prompt/question and it returns answer of the provided prompts.
     }
 }
